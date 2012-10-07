a03060aa3df5681c34ca64942036e858e00389c0
Daniel Beardsley <daniel@ifixit.com>
2012-10-06 17:19:22 -0700

    Modify dummy plugin to work with new API

--- plugins/dummy.js
+++ plugins/dummy.js
@@ -1,11 +1,10 @@
 exports.init = function(config, cimpler) {
-   cimpler.builds.pop(function(build, done) {
+   cimpler.consumeBuild(function(build, finished) {
       console.log("Starting Build");
       setTimeout(function() {
          console.dir(build);
          build.status = 'success';
-         cimpler.finishedBuilds.push(build);
-         done();
+         finished();
       }, 20000);
    });
 };
