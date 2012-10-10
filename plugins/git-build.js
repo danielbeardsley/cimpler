3871eebefea4143bc411e98e570200cca1fb65ca
Daniel Beardsley <daniel@ifixit.com>
2012-10-10 10:25:17 -0700

    GitBuild: separate merge and build steps
    * Report error messages more appropriately.
    * Include inherited environment variables in child process env.

--- plugins/git-build.js
+++ plugins/git-build.js
@@ -1,9 +1,10 @@
 var childProcess  = require('child_process'),
+    fs            = require('fs'),
     path          = require('path');
 
 exports.init = function(config, cimpler) {
    cimpler.consumeBuild(function(build, finished) {
-      var options = {
+      var execOptions = {
          env: {
             BUILD_REPO:   build.repo,
             BUILD_SHA:    build.sha,
@@ -11,7 +12,13 @@ exports.init = function(config, cimpler) {
             BUILD_STATUS: build.status
          },
          timeout: config.timeout || 0
-      };
+      },
+      logFilePath,
+      logRedirection = '';
+
+      for(var key in process.env) {
+         execOptions.env[key] = process.env[key];
+      }
 
       if (!config.repoPath) {
          throw new Error("Missing the 'path' option of git-build config");
@@ -19,7 +26,8 @@ exports.init = function(config, cimpler) {
 
       var logRedirection = "";
       if (config.logs.path) {
-         var logFilename = build.branch + "--" + build.sha + ".log",
+         var logFilename = build.branch + "--" + build.sha + ".log";
+
          logFilePath = path.join(config.logs.path, logFilename);
          logRedirection = ' 1>"' + logFilePath + '" 2>&1';
 
@@ -28,22 +36,61 @@ exports.init = function(config, cimpler) {
          }
       }
 
-      var commands =
-         '(cd "' + config.repoPath + '" && ' +
-         "git fetch --quiet && " +
-         "git reset --hard --quiet && " +
-         "git checkout --quiet $BUILD_SHA && " +
-         "git merge origin/master >/dev/null && " + 
-         "echo Merge Successful && " +
-         "(" + config.cmd + ") && " +
-         "echo Build Successful ) " + logRedirection;
+      console.log(build.sha + " -- Building with git");
 
-      console.log("Building with git");
+      startMerge();
 
-      var proc = childProcess.exec(commands, options,
-         function(err, stdout, stderr) {
+      function startMerge() {
+         var commands = 
+            '(set -v; set -x; printenv;' +
+            'cd "' + config.repoPath + '" && ' +
+            "gig fetch && " +
+            "git reset --hard && " +
+            "git checkout $BUILD_SHA && " +
+            "git merge origin/master) 2>&1";
+
+         exec(commands, function(err, stdout) {
+            if (err) {
+               var failed = "Merge Failed";
+               build.status = 'error';
+               build.error = failed;
+               stdout += "\n\n" + failed;
+               console.log(build.sha + " -- " + failed);
+               finished();
+            } else {
+               var success = "Merge Successful";
+               stdout += success;
+               console.log(build.sha + " -- " + success);
+               startBuild();
+            }
+
+            if (logFilePath) {
+               fs.writeFile(logFilePath, stdout);
+            }
+         });
+      }
+
+      function startBuild() {
+         var commands = "(" + config.cmd + ")" +
+            echoStatusCmd('Build') +
+            logRedirection;
+
+         exec(commands, function(err, stdout) {
             build.status = err ? 'failure' : 'success';
+            console.log(build.sha + " -- Build " + build.status);
             finished();
          });
+      }
+
+      function exec(cmd, callback) {
+         return childProcess.exec(cmd, execOptions,
+            function(err, stdout, stderr) {
+               callback(err, stdout.toString());
+            });
+      }
    });
+
+   function echoStatusCmd(noun) {
+      return " && echo '"+noun+" Successful' || ( echo '"+noun+" Failed'; exit 1 )";
+   }
 };
