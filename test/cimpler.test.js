7b248fe10bfd3f63e2019db5aa6228852bb591ee
Daniel Beardsley <daniel@ifixit.com>
2012-10-15 01:22:49 -0700

    Tests: convert cimpler test to mocha

--- test/cimpler.test.js
+++ test/cimpler.test.js
@@ -1,107 +1,126 @@
-var Cimpler  = require('../lib/cimpler'),
-    assert = require('assert');
+var Cimpler       = require('../lib/cimpler'),
+    dummyPlugin   = require('../plugins/dummy'),
+    assert        = require('assert');
 
-exports.registerPlugins = function(done) {
-   var dummyConfig = { a:1 },
-      config = {
-         plugins: {
-            dummy: dummyConfig,
-            notLoadedPlugin: {
-               enabled: false
-            }
+describe("Cimpler", function() {
+   describe(".registerPlugins()", function() {
+      var dummyConfig = { a:1 },
+         config = {
+            plugins: {
+               dummy: dummyConfig,
+               notLoadedPlugin: {
+                  enabled: false
+               }
+            },
+            testMode: true  // Don't console.log() anything
          },
-         testMode: true  // Don't console.log() anything
-      };
+         cimpler;
 
-   var cimpler = new Cimpler(config);
+      before(function() {
+         cimpler = new Cimpler(config);
+      });
 
-   assert.equal(cimpler.plugins.length, 1);
-   var dummy = require('../plugins/dummy');
-   assert.equal(dummy, cimpler.plugins[0]);
-   assert.equal(dummyConfig, cimpler.plugins[0].config);
-};
-
-exports.registerPlugin = function(done) {
-   var config = {a: 1},
-   cb = false,
-   cimpler = new Cimpler();
-  
-   cimpler.registerPlugin({
-      init: function(inConfig, inCimpler) {
-         cb = true;
-         assert.equal(inConfig, config);
-         assert.equal(inCimpler, cimpler);
-      }
-   }, config);
-
-   done(function() {
-      assert.ok(cb);
-   });
-};
-
-exports['build events'] = function(done) {
-   var build = {a: 1},
-   cb = [],
-   cimpler = new Cimpler();
-  
-   cimpler.on('buildAdded', function(inBuild) {
-      assert.equal(inBuild, build);
-      cb.push('added');
-   });
-   cimpler.addBuild(build);
-   cimpler.on('buildStarted', function(inBuild) {
-      assert.equal(inBuild, build);
-      cb.push('started');
-   });
-   cimpler.consumeBuild(function(inBuild, started, finished) {
-      assert.equal(inBuild, build);
-      cb.push('consumed');
-      process.nextTick(function() {
-         started();
-         process.nextTick(function() { finished();});
+      it("should load and instantiate the correct plugins modules", function() {
+         assert.equal(cimpler.plugins.length, 1);
+         assert.equal(dummyPlugin, cimpler.plugins[0]);
+         assert.equal(dummyConfig, cimpler.plugins[0].config);
       });
    });
-   cimpler.on('buildFinished', function(inBuild) {
-      assert.equal(inBuild, build);
-      cb.push('finished');
+
+   describe(".registerPlugin()", function() {
+      it("should call plugin.init and pass the config", function(done) {
+         var config = {a: 1},
+         cb = false,
+         cimpler = new Cimpler();
+
+         cimpler.registerPlugin({
+            init: function(inConfig, inCimpler) {
+               assert.equal(inConfig, config);
+               assert.equal(inCimpler, cimpler);
+               done();
+            }
+         }, config);
+      });
    });
 
-   done(function() {
-      assert.deepEqual(cb, ['added', 'consumed', 'started', 'finished']);
-   });
-};
+   describe("build events", function() {
+      it("should be emitted in the correct order", function(done) {
+         var build = {a: 1},
+         cb = [],
+         cimpler = new Cimpler();
 
-exports.consumeMultipleBuilds = function(done, assert) {
-   var first = {f: 1},
-   second = {s: 1},
-   cb = 0,
-   cimpler = new Cimpler();
-  
-   cimpler.consumeBuild(function(inBuild, started, finished) {
-      assert.equal(inBuild, cb === 0 ? first : second);
-      cb++;
-      finished();
-   });
-   cimpler.addBuild(first);
-   cimpler.addBuild(second);
+         var check = expect(4, function() {
+            assert.deepEqual(cb, ['added', 'consumed', 'started', 'finished']);
+            done();
+         });
 
-   done(function() {
-      assert.equal(cb, 2);
-   });
-};
+         cimpler.on('buildAdded', function(inBuild) {
+            assert.equal(inBuild, build);
+            cb.push('added');
+            check();
+         });
+         cimpler.addBuild(build);
+         cimpler.on('buildStarted', function(inBuild) {
+            assert.equal(inBuild, build);
+            cb.push('started');
+            check();
+         });
+         cimpler.consumeBuild(function(inBuild, started, finished) {
+            assert.equal(inBuild, build);
+            cb.push('consumed');
+            check();
+            process.nextTick(function() {
+               started();
+               process.nextTick(function() { finished();});
+            });
+         });
+         cimpler.on('buildFinished', function(inBuild) {
+            assert.equal(inBuild, build);
+            cb.push('finished');
+            check();
+         });
 
-exports.shutdown = function(done) {
-   var cb = 0,
-   cimpler = new Cimpler();
-  
-   cimpler.on('shutdown', function() {
-      cb++;
+         function expect(count, callback) {
+            return function() {
+               if (--count == 0) {
+                  callback();
+               }
+            }
+         };
+      });
    });
-   cimpler.shutdown();
-   // Ensure it only gets called once
-   cimpler.shutdown();
 
-   done(function() {
-      assert.equal(cb, 1);
+   describe(".consumeBuild()", function() {
+      it("should work with multiple builds", function(done) {
+         var first = {f: 1},
+         second = {s: 1},
+         cb = 0,
+         cimpler = new Cimpler();
+
+         cimpler.consumeBuild(function(inBuild, started, finished) {
+            assert.equal(inBuild, cb === 0 ? first : second);
+            finished();
+            cb++;
+            if (cb >= 2) done();
+         });
+
+         cimpler.addBuild(first);
+         cimpler.addBuild(second);
+      });
    });
-};
+
+   describe(".shutdown()", function() {
+      it("should emit the shutdown event (only once)", function() {
+         var cb = 0,
+         cimpler = new Cimpler();
+
+         cimpler.on('shutdown', function() { cb++; });
+         cimpler.shutdown();
+         // Ensure it only gets called once
+         cimpler.shutdown();
+
+         // Ensure it's called immediately.
+         assert.equal(cb, 1);
+      });
+   });
+});
