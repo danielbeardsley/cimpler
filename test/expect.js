10e0ac396c0906e320d43882756282a0db40e457
Daniel Beardsley <daniel@ifixit.com>
2012-10-19 11:25:55 -0700

    Tests: Improve expect() module
    Make it fail if you call it MORE times than you intended.

--- test/expect.js
+++ test/expect.js
@@ -1,12 +1,18 @@
+var assert = require('assert');
 /**
  * Simple utility to call the callback
  * after the returned function ahs been called *count* times
  */
 module.exports =
-   function expect(count, callback) {
+   function expect(originalCount, callback) {
+      var count = originalCount
       return function() {
          if (--count == 0) {
-            callback();
+            // Delay a bit to see if any more events come through
+            setTimeout(callback, 100);
+         } else if (count < 0) {
+            assert.fail("Got " + (originalCount + 1) +
+             " events, expecting " + originalCount);
          }
       }
    };
